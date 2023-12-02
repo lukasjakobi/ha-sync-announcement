19fcad9cb02b351dd84dd61b682ef03633c3792f
Lukas Jakobi <lukas@jakobi.io>
2023-12-02 01:43:46 +0100

    add lint github action & fix lint issues (#2)
    add lint github action & fix lint issues

--- python_scripts/sync_announcement.py
+++ python_scripts/sync_announcement.py
@@ -4,33 +4,41 @@ media_players = data.get('media_players')
 # the message to play
 message = data.get('message')
 
-# optional - use caching mechanisms (true/false, default: true)
+# use caching mechanisms
+# optional - true/false, default: true
 cache = data.get('cache') or True
 
-# optional - specify the language for the tts model (default: en-US)
+# specify the language for the tts model
+# optional - default: en-US
 language = data.get('language') or 'en-US'
 
-# optional - specify a volume to set to all media_players before playing the tts message (min: 0, max: 1, step: 0.01, default: None)
+# specify a volume to set to all media_players before playing the tts message
+# optional - min: 0, max: 1, step: 0.01, default: None
 volume = data.get('volume') or None
 
-# optional - specify if the volume should be reset to the previous state after the tts message (true/false, default: false)
+# specify if the volume should be reset to the previous state after the tts
+# message
+# optional - true/false, default: false
 volume_reset = data.get('volume_reset') or False
 
-# store the volume level of all media_players before running the script if specified
-if volume_reset == True:
+# store the volume level of all media_players before running the script if
+# specified
+if volume_reset:
     media_player_states_before_run = {}
     for entity_id in media_players:
-        media_player_states_before_run[entity_id] = hass.states.get(entity_id).attributes['volume_level']
+        media_player_states_before_run[entity_id] = (
+            hass.states.get(entity_id).attributes['volume_level']
+        )
 
 # set volume to all media_players if specified
-if volume != None:
+if volume is not None:
     hass.services.call('media_player', 'volume_set', {
         'entity_id': media_players,
         'volume_level': volume,
     })
 
 # send message through tts.cloud_say
-hass.services.call('tts','cloud_say', {
+hass.services.call('tts', 'cloud_say', {
     'entity_id': media_players,
     'message': message,
     'cache': cache,
@@ -38,7 +46,7 @@ hass.services.call('tts','cloud_say', {
 })
 
 # set respective previous volume to all media_players if specified
-if volume_reset == True:
+if volume_reset:
     for entity_id, previous_volume in media_player_states_before_run.items():
         hass.services.call('media_player', 'volume_set', {
             'entity_id': entity_id,
